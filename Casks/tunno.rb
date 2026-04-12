1f9b2ef3b8ed1b1f2985c02e5e8b78fe6e627965
Sergey Nikiforov <s.gelanor@gmail.com>
2026-04-12 21:43:08 +0400

    release: Tunno 1.0.0

--- Casks/tunno.rb
+++ Casks/tunno.rb
@@ -1,6 +1,6 @@
 cask "tunno" do
   version "1.0.0"
-  sha256 "d0e19a019657a550c0f68b95a5ef7e563862ba774c873b68cc3e3f14bcb09285"
+  sha256 "c7a273e1355da537b35984c3bf869e617d4ca1611fca94ed4affc14ecb907549"
 
   url "https://github.com/tunnoio/tunno/releases/download/v#{version}/Tunno-#{version}.dmg"
   name "Tunno"
